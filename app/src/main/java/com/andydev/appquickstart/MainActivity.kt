edeff9cbe57af8eff93fb571e4b8e22e901b08a5
Fazet <contact0faztweb.com>
2022-09-28 00:26:55 -0500

    hicimos un valores y variables

--- app/src/main/java/com/andydev/appquickstart/MainActivity.kt
+++ app/src/main/java/com/andydev/appquickstart/MainActivity.kt
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+
+    val hola: String = "Hola"
+    var adios: String = "Adios"
+
+    var numero = 2
+
+    //var c: Int
+
+
+    //val a: Int = 1 // immediate assignment
+    //val b: 2 // ´Int´ type is inferred
+    //val c: Int // type required when no initializar is provided
+    //c = 3 // deferred assignment
+
+    //var x = 5 //´Int´ type is inferred
+    //x += 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.v("hl","hola gente")
+        adios = "hola"
+
+        Log.v(adios)
     }
 }
