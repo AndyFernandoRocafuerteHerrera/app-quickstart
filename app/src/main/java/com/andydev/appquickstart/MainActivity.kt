a82eba52dce534fb22a88157edfe01a089d0ea8d
Fazet <contact0faztweb.com>
2022-09-29 08:31:04 -0500

    funciones

--- app/src/main/java/com/andydev/appquickstart/MainActivity.kt
+++ app/src/main/java/com/andydev/appquickstart/MainActivity.kt
@@ -3,31 +3,23 @@ package com.andydev.appquickstart
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import java.sql.DriverManager.println
 
 class MainActivity : AppCompatActivity()
-
-    val hola: String = "Hola"
-    var adios: String = "Adios"
-
-    var numero = 2
-
-    //var c: Int
-
-
-    //val a: Int = 1 // immediate assignment
-    //val b: 2 // ´Int´ type is inferred
-    //val c: Int // type required when no initializar is provided
-    //c = 3 // deferred assignment
-
-    //var x = 5 //´Int´ type is inferred
-    //x += 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        adios = "hola"
-
-        Log.v(adios)
+        mostrarHola()
+        mostraradios()
     }
+
+    fun mostrarHola(){
+        println("HOLA KOTLIN!")
+    }
+
+    fun mostraradios() {
+        println("adios kotlin!")
+    }
+
 }
